99d01653f24f34db3bfd695c61b989a99be20acd
NiklasBecker <58000608+NiklasBecker@users.noreply.github.com>
2022-10-03 16:20:07 +0200

    changed leds

--- RGBBlink/main.py
+++ RGBBlink/main.py
@@ -5,9 +5,9 @@ pycom.heartbeat(False)
 
 while True:
     #colors in hexadecimal (0xRRGGBB)
-    pycom.rgbled(0xFF0000)  # Red
+    pycom.rgbled(0x110000)  # Red
     time.sleep(1)
-    pycom.rgbled(0x00FF00)  # Green
+    pycom.rgbled(0x001100)  # Green
     time.sleep(1)
-    pycom.rgbled(0x0000FF)  # Blue
+    pycom.rgbled(0x000011)  # Blue
     time.sleep(1)
