d244423877b5151305dcb436a2c98532f321c0ff
NiklasBecker <58000608+NiklasBecker@users.noreply.github.com>
2022-10-10 16:12:39 +0200

    library testing

--- RGBBlink/boot.py
+++ RGBBlink/boot.py
@@ -1,10 +1,10 @@
 # boot.py -- run on boot-up
-from network import WLAN
-import machine
-wlan = WLAN(mode=WLAN.STA)
+# from network import WLAN
+# import machine
+# wlan = WLAN(mode=WLAN.STA)
 
-wlan.connect(ssid='AndroidAP292C', auth=(WLAN.WPA2, 'olpo7642'))
-while not wlan.isconnected():
-    machine.idle()
-print("WiFi connected successfully")
-print(wlan.ifconfig())
+# wlan.connect(ssid='AndroidAP292C', auth=(WLAN.WPA2, 'olpo7642'))
+# while not wlan.isconnected():
+#     machine.idle()
+# print("WiFi connected successfully")
+# print(wlan.ifconfig())
