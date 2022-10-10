a72e33f188a141adf3f0cdc992b175b3ccd83a09
NiklasBecker <58000608+NiklasBecker@users.noreply.github.com>
2022-10-10 15:02:39 +0200

    test

--- RGBBlink/main.py
+++ RGBBlink/main.py
@@ -4,10 +4,9 @@ import time
 pycom.heartbeat(False)
 
 while True:
-    #colors in hexadecimal (0xRRGGBB)
-    pycom.rgbled(0x110000)  # Red
+    pycom.rgbled(0xFF0000)  # Red
     time.sleep(1)
-    pycom.rgbled(0x001100)  # Green
+    pycom.rgbled(0x00FF00)  # Green
     time.sleep(1)
-    pycom.rgbled(0x000011)  # Blue
+    pycom.rgbled(0x0000FF)  # Blue
     time.sleep(1)
