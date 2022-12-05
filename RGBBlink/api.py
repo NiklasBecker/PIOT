a6bc692cf9eca58428028553ec6d09a21417c06a
NiklasBecker <58000608+NiklasBecker@users.noreply.github.com>
2022-12-05 16:46:07 +0100

    Create Svelte Project for Website

--- RGBBlink/api.py
+++ RGBBlink/api.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from content_control import get_from_file
+import json
 
 app = FastAPI()
+data = get_from_file()
 
 @app.get("/fridge-items")
 def fridge_items():
-    return {"Hello TEST!"}
+    return json.dumps(data)
--- RGBBlink/main.py
+++ RGBBlink/main.py
@@ -16,20 +16,3 @@ contents = get_from_file()
 new_id = calc_new_id(contents)
 add_item_to_data(contents, new_id, product_name)
 write_to_file(contents)
-
-# # get data from json file
-# with open('lib/contents.json') as json_file:
-# data = json.load(json_file)
-
-# number_of_items = len(data['items'])
-
-# last_id_in_fridge = data['items'][number_of_items - 1]['item_id']
-
-# new_id = last_id_in_fridge + 1
-# data['items'].append({"item_id": new_id, "name": product_name})
-
-# # enter adapted data back into json file
-# with open('lib/contents.json', 'w') as outfile:
-# json.dump(data, outfile)
-
-# print(data)
