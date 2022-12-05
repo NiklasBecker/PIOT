1ee848424061bee0c2306b426ac4ccd59010d94c
NiklasBecker <58000608+NiklasBecker@users.noreply.github.com>
2022-12-05 14:55:01 +0100

    added contents json

--- RGBBlink/main.py
+++ RGBBlink/main.py
@@ -2,8 +2,9 @@
 import barcodereader
 import requests
 
-product_code = 9781911223139
+product_code = 3017620422003
 # barcodereader.BarcodeReader(Img.jpg)
 
 response = requests.get('https://world.openfoodfacts.org/api/v0/product/' + str(product_code) + '.json')
 print(response.json())
+print(response.json()['product']['product_name'])
