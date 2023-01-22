2c863af96a43885da6f9f94ab93328b39abcd5c9
NiklasBecker <58000608+NiklasBecker@users.noreply.github.com>
2023-01-22 11:32:19 +0100

    fixed api

--- FridgeWatch/api.py
+++ FridgeWatch/api.py
@@ -13,19 +13,19 @@ class Item(BaseModel):
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:5173"
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:8000",
+#     "http://localhost:5173"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.get("/")
 async def root():
